304ec9969ac227601620030ac3562bb2d908b119
kareemwally <kareemwally@std.mans.edu.eg>
2024-02-27 13:12:37 -0800

    insert at left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,6 +27,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		parent->left->parent = res;
 		res->left = parent->left;
+		parent->left = res;
 	}
 	return (res);
 }
