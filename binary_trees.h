2140a082d4c84e1b0c77f2d381396ebbdb38b7fc
kareemwally <kareemwally@std.mans.edu.eg>
2024-02-28 20:11:26 -0800

    the header file

--- binary_trees.h
+++ binary_trees.h
@@ -38,4 +38,5 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_print(const binary_tree_t *);
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
 #endif
