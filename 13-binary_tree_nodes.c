a4699e95da5d9cc5f4f143c50fe4b458c0d56662
kareemwally <kareemwally@std.mans.edu.eg>
2024-02-28 20:36:03 -0800

    1 or more child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,7 +9,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
-	if (tree->parent != NULL && (tree->left != NULL || tree->right != NULL))
-		return (1);
 	return (binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right));
 }
