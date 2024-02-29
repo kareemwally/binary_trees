9048d392ba60c482a92b520b705dd5a7ea82bc77
kareemwally <kareemwally@std.mans.edu.eg>
2024-02-29 12:12:34 -0800

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_height - Calculate the height of a binary tree
  * @tree: Pointer to the root node of the tree
@@ -11,37 +10,42 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
 
 /**
- * binary_tree_balance - Calculate the balance factor of a binary tree node
- * @tree: Pointer to the node to calculate the balance factor
- * Return: Balance factor of the node, or 0 if tree is NULL
+ * binary_tree_is_perfect - Check if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree
+ * Return: 1 if the tree is perfect, 0 otherwise
  */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
-}
-/**
- * binary_tree_is_perfect - Function
- * Description: to determine if tree is perfect or not
- * @tree: pointer to root node
- * Return: (int)
- */
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	size_t height = binary_tree_height(tree);
+
 	if (tree == NULL)
 		return (0);
-	if (binary_tree_balance(tree) == 0)
+	return (perfect_recursive(tree, height, 0));
+}
+
+/**
+ * perfect_recursive - Function
+ * Description: Check if a binary tree is perfect recursively
+ * @tree: Pointer to the root node of the tree
+ * @height: Height of the tree
+ * @level: Current level of the tree
+ * Return: 1 if the tree is perfect, 0 otherwise
+ */
+
+int perfect_recursive(const binary_tree_t *tree, size_t height, size_t level)
+{
+	if (tree == NULL)
 		return (1);
-	else
+	if (tree->left == NULL && tree->right == NULL)
+		return (height == level + 1);
+	if (tree->left == NULL || tree->right == NULL)
 		return (0);
+	return (perfect_recursive(tree->left, height, level + 1) &&
+		perfect_recursive(tree->right, height, level + 1));
 }
