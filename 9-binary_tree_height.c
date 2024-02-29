49bdef5beefc3a2bd501eb4b1d60dc1217a61b03
kareemwally <kareemwally@std.mans.edu.eg>
2024-02-28 18:46:46 -0800

    size_t is trash

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,10 +17,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	right_height = binary_tree_height(tree->right);
 	if (left_height > right_height)
 	{
-		return ((1+left_height));
+		return ((left_height + 1));
 	}
 	else
 	{
-		return ((1+right_height));
+		return ((right_height + 1));
 	}
 }
