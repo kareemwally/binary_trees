a3d6c6b1a6eee23cf9a373f0a7bea49e88765048
kareemwally <kareemwally@std.mans.edu.eg>
2024-02-28 19:54:32 -0800

    getting size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - Function
+ * Description: get the number of nodes in tree
+ * @tree: pointer to root node
+ * Return: (size_t)
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return(0);
+	}
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
